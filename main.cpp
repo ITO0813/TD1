3205fba0938f66314fb1ef96699aa747ba83cad1
hibiki <k022g1084@g.neec.ac.jp>
2023-11-08 14:08:59 +0900

    スクロールの実装

--- main.cpp
+++ main.cpp
@@ -12,6 +12,19 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	char keys[256] = {0};
 	char preKeys[256] = {0};
 
+	const int World_Width = 3840;
+
+	int background[4] = {0, 1280, 2560, 3860};
+
+	int posX = 40;
+	int posY = 560;
+	int posrad = 1;
+	int posW = 50;
+	int posH = 50;
+	int speed = 20;
+	int ScrollX = posX + posrad;
+	int playerScroll = posX - ScrollX;
+
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
 		// フレームの開始
@@ -24,6 +37,30 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓更新処理ここから
 		///
+
+		if (keys[DIK_D]) {
+			posX += speed;
+		}
+		if (keys[DIK_A]) {
+			posX -= speed;
+		}
+
+		if (posX < 0 + posrad) {
+			posX = 0 + posrad;
+		}
+		if (posX > World_Width - posrad) {
+			posX = World_Width - posrad;
+		}
+
+		ScrollX = posX + posrad;
+
+		if (ScrollX < 0) {
+			ScrollX = 0;
+		}
+		if (ScrollX > World_Width - 1280) {
+			ScrollX = World_Width - 1280;
+		}
+		playerScroll = posX - ScrollX;
 
 		///
 		/// ↑更新処理ここまで
