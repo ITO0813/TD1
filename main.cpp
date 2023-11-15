46b4439299187155aef0ca02c10bd18aa0c7bff8
hibiki <k022g1084@g.neec.ac.jp>
2023-11-15 14:47:30 +0900

    統合後問題なし

--- main.cpp
+++ main.cpp
@@ -86,8 +86,8 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	int rightBottomX = 0;
 	int rightBottomY = 0;
 
-	int blockImges1 = Novice::LoadTexture("./block4.png"); // ブロックの画像 　　マップ番号が1の時
-	int blockImges2 = Novice::LoadTexture("./TDblock.png"); // ブロックの画像　 マップ番号が2の時
+	int blockImges1 = Novice::LoadTexture("./images/block4.png"); // ブロックの画像 　　マップ番号が1の時
+	int blockImges2 = Novice::LoadTexture("./images/Obstacle.png"); // ブロックの画像　 マップ番号が2の時
 	int blockSize = 32;
 
 	int rightTopX = 0;
@@ -114,54 +114,30 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
     };
 
 	int map[24][40] = {
-	    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-	     1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-	    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-	     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-	    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-	     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-	    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-	     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-	    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-	     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-	    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-	     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-	    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0,
-	     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-	    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-	     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-	    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-	     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-	    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-	     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-	    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-	     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-	    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-	     0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-	    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-	     0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-	    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2,
-	     2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-	    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-	     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-	    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-	     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-	    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1,
-	     1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-	    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1,
-	     1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-	    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1,
-	     1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-	    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-	     1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-	    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-	     1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-	    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-	     1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-	    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-	     1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-	    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-	     1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+	    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+	    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+	    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+	    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+	    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+	    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+	    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+	    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+	    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
 	};
 
 	int time = 999;
@@ -187,69 +163,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓更新処理ここから
 		///
 
-
-		time -= 1;
-
-		if (time == 0) {
-			break;
-		}
-
-		// 当たり判定の変数
-		leftBottomX = ((int)ball.position.x - (int)ball.radius) / blockSize;
-		leftBottomY = ((int)ball.position.y + (int)ball.radius - 1) / blockSize;
-
-		rightBottomX = ((int)ball.position.x + (int)ball.radius - 1) / blockSize;
-		rightBottomY = ((int)ball.position.y + (int)ball.radius - 1) / blockSize;
-
-		rightTopX = ((int)ball.position.x + (int)ball.radius - 1) / blockSize;
-		rightTopY = ((int)ball.position.y - (int)ball.radius) / blockSize;
-		if (map[rightTopY][rightTopX] == 0 && map[rightBottomY][rightBottomX] == 0) {
-			ball.speed.x = 2;
-			ball.position.x = ball.position.x + ball.speed.x; // 本体を進める
-		}
-
-		if (isJump == 0) {
-			if (keys[DIK_SPACE] && preKeys[DIK_SPACE] == 0) {
-				ball.speed.y = -5.0f; // ジャンプ
-				isJump = 1;
-			}
-		} else if (isFly == 0 && isJump == 1) {
-			if (preKeys[DIK_SPACE] && keys[DIK_SPACE]) {
-				ball.jump.y = 0.1f;
-				isFly = 1;
-			} else {
-				ball.jump.y = 0.8f;
-			}
-		}
-
-		ball.speed.y += ball.jump.y; // 重力を加算
-
-		ball.position.y += ball.speed.y; // プレイヤーに重力を加算
-		rightBottomX = ((int)ball.position.x + (int)ball.radius - 1) / blockSize;
-		rightBottomY =
-		    ((int)ball.position.y + (int)ball.radius - 1 + (int)ball.speed.y) / blockSize;
-
-		leftBottomX = ((int)ball.position.x - (int)ball.radius) / blockSize;
-		leftBottomY = ((int)ball.position.y + (int)ball.radius - 1 + (int)ball.speed.y) / blockSize;
-
-		rightTopX = ((int)ball.position.x + (int)ball.radius - 1) / blockSize;
-		rightTopY = ((int)ball.position.y - (int)ball.radius + (int)ball.speed.y) / blockSize;
-		leftTopX = ((int)ball.position.x - (int)ball.radius) / blockSize;
-		leftTopY = ((int)ball.position.y - (int)ball.radius + (int)ball.speed.y) / blockSize;
-
-		if (map[rightTopY][rightTopX] != 0 && map[leftTopY][leftTopX] != 0) {
-
-			ball.speed.y = 0.0f;
-			isJump = 0;
-			isFly = 0;
-		}
-		if (map[rightBottomY][rightBottomX] != 0 && map[leftBottomY][leftBottomX] != 0) {
-			ball.position.y = (rightBottomY * blockSize) - ball.radius;
-			ball.speed.y = 0.0f;
-			isJump = 0;
-			isFly = 0;
-		}
-
 		int nextScene = scene;
 
 		// タイトルシーンの時
@@ -271,6 +184,70 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		// メインシーンの時
 		else if (scene == mainscene) {
 			nextScene = Mainscene(keys, preKeys, mainbackgroundHandle);
+
+			time -= 1;
+
+			if (time == 0) {
+				break;
+			}
+
+			// 当たり判定の変数
+			leftBottomX = ((int)ball.position.x - (int)ball.radius) / blockSize;
+			leftBottomY = ((int)ball.position.y + (int)ball.radius - 1) / blockSize;
+
+			rightBottomX = ((int)ball.position.x + (int)ball.radius - 1) / blockSize;
+			rightBottomY = ((int)ball.position.y + (int)ball.radius - 1) / blockSize;
+
+			rightTopX = ((int)ball.position.x + (int)ball.radius - 1) / blockSize;
+			rightTopY = ((int)ball.position.y - (int)ball.radius) / blockSize;
+			if (map[rightTopY][rightTopX] == 0 && map[rightBottomY][rightBottomX] == 0) {
+				ball.speed.x = 2;
+				ball.position.x = ball.position.x + ball.speed.x; // 本体を進める
+			}
+
+			if (isJump == 0) {
+				if (keys[DIK_SPACE] && preKeys[DIK_SPACE] == 0) {
+					ball.speed.y = -5.0f; // ジャンプ
+					isJump = 1;
+				}
+			} else if (isFly == 0 && isJump == 1) {
+				if (preKeys[DIK_SPACE] && keys[DIK_SPACE]) {
+					ball.jump.y = 0.1f;
+					isFly = 1;
+				} else {
+					ball.jump.y = 0.8f;
+				}
+			}
+
+			ball.speed.y += ball.jump.y; // 重力を加算
+
+			ball.position.y += ball.speed.y; // プレイヤーに重力を加算
+			rightBottomX = ((int)ball.position.x + (int)ball.radius - 1) / blockSize;
+			rightBottomY =
+			    ((int)ball.position.y + (int)ball.radius - 1 + (int)ball.speed.y) / blockSize;
+
+			leftBottomX = ((int)ball.position.x - (int)ball.radius) / blockSize;
+			leftBottomY =
+			    ((int)ball.position.y + (int)ball.radius - 1 + (int)ball.speed.y) / blockSize;
+
+			rightTopX = ((int)ball.position.x + (int)ball.radius - 1) / blockSize;
+			rightTopY = ((int)ball.position.y - (int)ball.radius + (int)ball.speed.y) / blockSize;
+			leftTopX = ((int)ball.position.x - (int)ball.radius) / blockSize;
+			leftTopY = ((int)ball.position.y - (int)ball.radius + (int)ball.speed.y) / blockSize;
+
+			if (map[rightTopY][rightTopX] != 0 && map[leftTopY][leftTopX] != 0) {
+
+				ball.speed.y = 0.0f;
+				isJump = 0;
+				isFly = 0;
+			}
+			if (map[rightBottomY][rightBottomX] != 0 && map[leftBottomY][leftBottomX] != 0) {
+				ball.position.y = (rightBottomY * blockSize) - ball.radius;
+				ball.speed.y = 0.0f;
+				isJump = 0;
+				isFly = 0;
+			}
+
 		}
 		if (scene != nextScene) {
 			scene = nextScene;
@@ -300,26 +277,23 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓描画処理ここから
 		///
 		 
-		Novice::DrawEllipse(
-		    (int)ball.position.x, (int)ball.position.y, (int)ball.radius, (int)ball.radius, 0.0f,
-		    RED, kFillModeSolid);
+		if (scene == mainscene) {
+			Novice::DrawEllipse((int)ball.position.x, (int)ball.position.y, (int)ball.radius, (int)ball.radius,0.0f, RED, kFillModeSolid);
 
-		// kFillModeSolid);//プレイヤー
-		for (int i = 0; i < mapY; i++) {
-			for (int p = 0; p < mapX; p++) {
-				if (map[i][p] == 1) { // マップ番号が1の時
-					Novice::DrawSprite(
-					    (p * blockSize), i * blockSize, blockImges1, 1.0f, 1.0f, 0.0f, WHITE);
-				}
-				if (map[i][p] == 2) { // マップ番号が2の時
-					Novice::DrawSprite(
-					    (p * blockSize), i * blockSize, blockImges2, 1.0f, 1.0f, 0.0f, WHITE);
+			// kFillModeSolid);//プレイヤー
+			for (int i = 0; i < mapY; i++) {
+				for (int p = 0; p < mapX; p++) {
+					if (map[i][p] == 1) { // マップ番号が1の時
+						Novice::DrawSprite(
+						    (p * blockSize), i * blockSize, blockImges1, 1.0f, 1.0f, 0.0f, WHITE);
+					}
+					if (map[i][p] == 2) { // マップ番号が2の時
+						Novice::DrawSprite(
+						    (p * blockSize), i * blockSize, blockImges2, 1.0f, 1.0f, 0.0f, WHITE);
+					}
 				}
 			}
 		}
-
-		Novice::ScreenPrintf(100, 100, "%d", scene);
-
 
 		///
 		/// ↑描画処理ここまで
